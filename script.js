9248b60b257618a2f5d18d824eea232912856a51
Deepak0090 <167174586+Deepak0090@users.noreply.github.com>
2025-01-24 16:03:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+() => {
+  const content = "Before Element";
+  cy.window().then(window => {
+    cy.get('a.before').then(element => {
+      const before = window.getComputedStyle(element[0], "::before");
+      const color = before.getPropertyValue("color");
+      expect(color).to.equal('rgb(255, 0, 0)');
+      const beforeContent = before.getPropertyValue("content");
+      expect(beforeContent).to.equal(`"${content}"`);
+    });
+  });
+}
